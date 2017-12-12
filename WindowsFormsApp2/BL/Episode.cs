66cc3904f1cff40b2bba13203a7d5d6aa6345b95
Eric Känngård <P3UR3@live.se>
2017-12-12 15:35:49 +0100

    update

--- WindowsFormsApp2/BL/Episode.cs
+++ WindowsFormsApp2/BL/Episode.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp2.BL
         public String link { get; set; }
         public String description { get; set; }
         public bool isRead { get; set; }
-
+         
         public Episode(string title, string link, string description, bool isRead)
         {
             this.title = title;
